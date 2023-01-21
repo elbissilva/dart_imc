b754ac76ef19a1da536c3b962a085fc859eaba58
Elbis Silva <elbissilva@gmail.comm>
2023-01-21 17:55:55 -0300

    commit4

--- lib/dart_imc.dart
+++ lib/dart_imc.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
-import 'dart:svg';
-import 'dart_imc.dart';
+
 
 
 
@@ -12,20 +11,35 @@ void runAapp() {
   var altura = lerConsoleDouble("Informe sua altura");
 
   var peso = lerConsoleDouble("Informe seu peso");
-
-  var imc = peso /( altura*altura);
+  imcCalc(peso, altura);
   
 
    
-  if (imc < 18.5) {
-    print("$nome Você está abaixo do peso com o imc de $imc}");
-  } else if (imc> 18.5 && imc <=24.9) {
-      print("$nome Você está com o peso normal para sua altura, e seu imc é $imc");
-  } else if (imc >25.0 && imc <29.9){
-      print("$nome Você está com sobrepeso, com o imc de ${imc.}");
-  } else if (imc >= 30.0 ){
-      print("$nome Você está obeso, com imc de $imc");
+  if (imcCalc(peso, altura) < 16) {
+    print("$nome Você está abaixo do peso com imc de: ${imcCalc(peso, altura).toStringAsFixed(2)}, considerado MAGREZA GRAVE!");
+
+  } else if (imcCalc(peso, altura)> 16 && imcCalc(peso, altura) <=17) {
+      print("$nome Você está abaixo do peso com imc de: ${imcCalc(peso, altura).toStringAsFixed(2)} considerado 'MAGREZA MODERADA");
+
+  } else if (imcCalc(peso, altura) >17 && imcCalc(peso, altura) <18.5){
+      print("$nome Você está abaixo do peso com imc de:  ${imcCalc(peso, altura).toStringAsFixed(2)}considerado 'MAGREZA LEVE");
+
+  } else if (imcCalc(peso, altura) >18.5 && imcCalc(peso, altura) <25){
+      print("$nome Você está com peso normal,  e o imc de  ${imcCalc(peso, altura).toStringAsFixed(2)} Considerado 'SAUDÁVEL");
+
+  } else if (imcCalc(peso, altura) >25.0 && imcCalc(peso, altura) <29.9){
+      print("$nome Você está acima do peso, com o imc de  ${imcCalc(peso, altura).toStringAsFixed(2)} considerado 'SOBREPESO");
+
+  } else if (imcCalc(peso, altura) >= 30.0 && imcCalc(peso, altura)<= 34.9 ){
+      print("$nome Você está acima do peso, com imc de  ${imcCalc(peso, altura).toStringAsFixed(2)} considerado 'OBESIDADE GRAU I");
+
+  } else if (imcCalc(peso, altura) >= 35.0 && imcCalc(peso, altura)<= 39.9 ){
+      print("$nome Você está acima do peso, com imc de  ${imcCalc(peso, altura).toStringAsFixed(2)} considerado 'OBESIDADE GRAU II(SEVERA)");
+
+  } else if (imcCalc(peso, altura) >= 40 ){
+      print("$nome Você está acima do peso, com imc de  ${imcCalc(peso, altura).toStringAsFixed(2)} considerado 'OBESIDADE GRAU III(MÓRBIDA)");
   }
+  
   
 }
 
@@ -44,6 +58,8 @@ lerConsoleDouble(String texto){
     return numero;
   }
 
- 
-  
+}
+double imcCalc(double peso, double altura){
+  var imc = peso /(altura*altura);
+  return imc;
 }
